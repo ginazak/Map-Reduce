9184fb40096f97a0317247f3f91275c010353ccb
Mary Gina Zacharias <55036152+ginazak@users.noreply.github.com>
2021-05-12 08:30:06 +0100

    Update and rename part1.py to max_value.py

--- max_value.py
+++ max_value.py
@@ -1,5 +1,6 @@
 """ This program will take a CSV data file, 
-and output the maximum value of all the numbers.
+and output the maximum value of all the numbers
+using the map-reduce paradigm.
 To run:
     pipenv run python part1.py fileName.csv
 """
