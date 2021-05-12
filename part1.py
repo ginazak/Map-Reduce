5111f215971c8cc061924a3440834bd58dad73de
Mary Gina Zacharias <55036152+ginazak@users.noreply.github.com>
2021-05-12 08:28:34 +0100

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,21 @@
+""" This program will take a CSV data file, 
+and output the maximum value of all the numbers.
+To run:
+    pipenv run python part1.py fileName.csv
+"""
+
+from mrjob.job import MRJob
+import csv
+
+class maxValue(MRJob):
+    
+    def mapper(self, key, document):
+        """ Extracts each number individually """
+        for number in document.split(","):
+            yield "_", float(number)
+
+    def reducer(self, _ , document):
+        """  Finds the max value of all the extracted numbers """
+        yield "_", max(document)
+
+maxValue.run()
